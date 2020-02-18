de61beff57781deb026fadccdf65e538b476ff77
Andrei Avris <andrei.avris@softvision.ro>
2020-02-18 09:22:35 +0200

    First commit

---
+++ src/test/java/ro/andrei/jersey/sbservice/sbservice/SbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.andrei.jersey.sbservice.sbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
